0475ee9db1c10ac100d10a0070bd48d973a69520
Igor0Ruan <igor.ruangarcia@gmail.com>
2019-06-21 14:15:28 -0300

    Implementação de view de consultas

--- src/app/barra-navegacao/barra-navegacao.component.ts
+++ src/app/barra-navegacao/barra-navegacao.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-barra-navegacao',
-  templateUrl: './barra-navegacao.component.html',
-  styleUrls: ['./barra-navegacao.component.css']
+    selector: 'app-barra-navegacao',
+    templateUrl: './barra-navegacao.component.html',
+    styleUrls: ['./barra-navegacao.component.css']
 })
 export class BarraNavegacaoComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
